20614621c596123775e15dc0652e90aaa3bb37e1
yorifuji <yorifuji@gmail.com>
2018-12-18 10:26:54 +0900

    add key.js

---
+++ key.js
@@ -0,0 +1,3 @@
+window.__SKYWAY_KEY__ = "dcffd3f8-3ce6-4e88-ab3d-97eca4d871f8"
+window.__TRANSLATE_URL__ = "https://script.google.com/macros/s/AKfycbw9Ss8FlBn_gwwH8GLMHj8kt03oLOdrSphtyz4O5tBpDqGEBto/exec";
+
